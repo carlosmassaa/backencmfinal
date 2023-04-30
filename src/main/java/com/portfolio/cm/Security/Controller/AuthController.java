61f9863b06e07842ad246fc1526500088911966d
carlosmassaa <carlosmb55555@gmail.com>
2023-04-30 11:47:29 -0300

    Solucionando render 14

--- src/main/java/com/portfolio/cm/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/cm/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://portfolio-frontend-cm.web.app")
+@CrossOrigin(origins = {"https://portfolio-frontend-cm.web.app"})
 public class AuthController {
     @Autowired 
     PasswordEncoder passwordEncoder;
